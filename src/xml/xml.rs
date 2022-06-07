1b7c63e695f586bd026e231a4f561a258965eb2e
u-kai <76635578+u-kai@users.noreply.github.com>
2022-06-07 20:22:34 +0900

    clear get_elements_by_key_value_test

--- src/xml/xml.rs
+++ src/xml/xml.rs
@@ -16,6 +16,18 @@ impl<'a, T: NodeInterface<'a>> XmlTree<'a, T> {
             self.children = Some(Box::new(vec![child]))
         }
     }
+    pub fn get_elements_by_key_value(&self, key: &str, value: &str) -> Vec<&XmlTree<'a, T>> {
+        let mut result = vec![];
+        if self.node.contains_key_value(key, value) {
+            result.push(self);
+        }
+        if self.children.is_some() {
+            self.children.as_ref().unwrap().iter().for_each(|child| {
+                result.extend(child.get_elements_by_key_value(key, value));
+            });
+        }
+        result
+    }
     pub fn get_elements_by_node_value(&self, value: &str) -> Vec<&XmlTree<'a, T>> {
         let mut result = vec![];
         if self.node.value() == value {
@@ -32,7 +44,36 @@ impl<'a, T: NodeInterface<'a>> XmlTree<'a, T> {
 #[cfg(test)]
 
 mod xml_tree_tests {
+    use crate::xml::nodes::node_interface::PropertyInterface;
+
     use super::{mock_node::MockNode, XmlTree};
+    #[test]
+    fn get_elements_by_key_value_test() {
+        let mut root = XmlTree {
+            node: MockNode::new("root"),
+            children: None,
+            _marker: Default::default(),
+        };
+        let mut child = MockNode::new("child");
+        child.add_property("id", "kai");
+        child.add_property("class", "style1");
+        child.add_property("class", "style2");
+        let child = XmlTree {
+            node: child,
+            children: None,
+            _marker: Default::default(),
+        };
+        root.append_children(child.clone());
+        assert_eq!(root.get_elements_by_key_value("id", "kai"), vec![&child]);
+        assert_eq!(
+            root.get_elements_by_key_value("class", "style1"),
+            vec![&child]
+        );
+        assert_eq!(
+            root.get_elements_by_key_value("class", "style2"),
+            vec![&child]
+        );
+    }
     #[test]
     fn get_elements_by_node_value_test() {
         let root = XmlTree {
@@ -103,6 +144,8 @@ mod xml_tree_tests {
 }
 
 mod mock_node {
+    use std::collections::HashMap;
+
     use crate::xml::nodes::node_interface::{
         ElementInterface, NodeInterface, PropertyInterface, PropertyKey, PropertyValue,
     };
@@ -110,10 +153,14 @@ mod mock_node {
     #[derive(Clone, Debug, PartialEq, Eq)]
     pub(super) struct MockNode<'a> {
         value: &'a str,
+        key_value: HashMap<String, Vec<String>>,
     }
     impl<'a> MockNode<'a> {
         pub fn new(value: &'a str) -> Self {
-            MockNode { value }
+            MockNode {
+                value,
+                key_value: HashMap::new(),
+            }
         }
     }
     impl<'a> ElementInterface<'a> for MockNode<'a> {
@@ -125,14 +172,37 @@ mod mock_node {
         }
     }
     impl<'a> PropertyInterface for MockNode<'a> {
-        fn add_property(&mut self, key: &str, value: &str) -> () {}
+        fn add_property(&mut self, key: &str, value: &str) -> () {
+            if self.key_value.contains_key(key) {
+                self.key_value
+                    .get_mut(key)
+                    .as_mut()
+                    .unwrap()
+                    .push(value.to_string());
+            } else {
+                self.key_value
+                    .insert(key.to_string(), vec![value.to_string()]);
+            }
+        }
         fn contains_key(&self, key: &str) -> bool {
-            true
+            self.key_value.contains_key(key)
         }
         fn contains_key_value(&self, key: &str, value: &str) -> bool {
-            true
+            if self.key_value.contains_key(key) {
+                self.key_value
+                    .get(key)
+                    .unwrap()
+                    .contains(&value.to_string())
+            } else {
+                false
+            }
         }
         fn keys(&self) -> Option<&Vec<PropertyKey>> {
+            //if self.key_value.keys().len() == 0 {
+            //None
+            //} else {
+            //self.key_value.keys()
+            //}
             None
         }
         fn values(&self) -> Option<Vec<Vec<PropertyValue>>> {
